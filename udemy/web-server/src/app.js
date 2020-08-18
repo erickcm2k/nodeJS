aca5a8982dcab1115c2cd0b0671479c542e0f698
Erick Castañeda Martínez <erickce40@gmail.com>
2020-08-17 23:17:30 -0500

    finished web

--- udemy/web-server/src/app.js
+++ udemy/web-server/src/app.js
@@ -1,31 +1,41 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
+
+// Creates an Express application.
 const app = express();
 
-// Getting address from public directory.
+// Define paths for Express config.
 const publicDirectory = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-// Using the hbs module for rendering views.
+// Setup handlebars engine and views location.
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectory));
 
 app.get('/', (req, res) => {
     // Renders one view.
     res.render('index', {
-        title: 'weather',
+        title: 'Main Section',
         name: 'Erick'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'Amazing Company',
-        name: 'me'
+        title: 'About Us Section',
+        name: 'Erick'
     });
 });
 
 app.get('/help', (req, res) => {
     res.render('help', {
+        title: 'Help Section',
         message: 'We are here for helping you',
         name: 'Erick'
     });
@@ -35,6 +45,21 @@ app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is snowing',
         location: 'philadelphia'
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Erick',
+        errorMessage: 'Help article not found.'
+    });});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Erick',
+        errorMessage: 'Page not found.'
     });
 });
 
