e54a2b51389c273fc57d737d4da3733bd46f1641
Erick Castañeda Martínez <erickce40@gmail.com>
2020-08-18 19:18:39 -0500

    integrating forecast and geocode utilities. Start using ESlint

--- udemy/web-server/src/app.js
+++ udemy/web-server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 // Creates an Express application.
 const app = express();
@@ -19,57 +21,87 @@ hbs.registerPartials(partialsPath);
 app.use(express.static(publicDirectory));
 
 app.get('/', (req, res) => {
-    // Renders one view.
-    res.render('index', {
-        title: 'Main Section',
-        name: 'Erick'
-    });
+  // Renders one view.
+  res.render('index', {
+    title: 'Main Section',
+    name: 'Erick',
+  });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Us Section',
-        name: 'Erick'
-    });
+  res.render('about', {
+    title: 'About Us Section',
+    name: 'Erick',
+  });
 });
 
 app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'Help Section',
-        message: 'We are here for helping you',
-        name: 'Erick'
-    });
+  res.render('help', {
+    title: 'Help Section',
+    message: 'We are here for helping you',
+    name: 'Erick',
+  });
 });
 
 app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'It is snowing',
-        location: 'philadelphia'
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide a search term',
     });
+  }
+
+  const { address } = req.query;
+
+  geocode(address, (error, data) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    const coordinates = `${data.latitude},${data.longitude}`;
+    forecast(coordinates, (error, data) => {
+      if (error) {
+        console.log('Error:', error);
+      } else {
+        console.log('Data:', data);
+        const { temperature, feelslike, location } = data;
+        res.send({
+          forecast: `Current temperature is ${temperature}, but feels like ${feelslike}`,
+          location,
+          address,
+        });
+      }
+    });
+  });
 });
 
-app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'It is snowing',
-        location: 'philadelphia'
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term',
     });
+  }
+
+  res.send({
+    products: [],
+  });
 });
 
 app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Erick',
-        errorMessage: 'Help article not found.'
-    });});
+  res.render('404', {
+    title: '404',
+    name: 'Erick',
+    errorMessage: 'Help article not found.',
+  });
+});
 
 app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Erick',
-        errorMessage: 'Page not found.'
-    });
+  res.render('404', {
+    title: '404',
+    name: 'Erick',
+    errorMessage: 'Page not found.',
+  });
 });
 
 app.listen(3000, () => {
-    console.log('Start listening on port http://localhost:3000.')
+  console.log('Start listening on port http://localhost:3000.');
 });
---
+++ udemy/web-server/src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('postman-request');
+const chalk = require('chalk');
+
+const forecast = (address, callback) => {
+  const WEATHER_STACK_API = `http://api.weatherstack.com/current?access_key=bcd6cc03ddb33d8629ed8df163d14bda&query=${address}`;
+  request({ url: WEATHER_STACK_API, json: true }, (error, response) => {
+    if (error) {
+      callback('Unable to connect to weatherstack.com API.', undefined);
+    } else if (response.body.error) {
+      callback('Weatherstack.com: Unable to find location. Check your querry.', undefined);
+    } else {
+      callback(undefined, {
+        location: `${response.body.location.name}, ${response.body.location.country}, ${response.body.location.region}`,
+        temperature: response.body.current.temperature,
+        feelslike: response.body.current.feelslike,
+      });
+    }
+  });
+};
+
+module.exports = forecast;
