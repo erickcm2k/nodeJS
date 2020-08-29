2c8318b4043c682c4815136e6bfb610a0f79967c
Erick Castañeda Martínez <erickce40@gmail.com>
2020-08-28 22:33:32 -0500

    storing on mongodb

--- nodeJS-course/task-manager/src/index.js
+++ nodeJS-course/task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000; // Port for Heroku deployment
@@ -12,7 +13,36 @@ app.post('/users', (req, res) => {
   user.save().then((result) => {
     res.send(result);
   }).catch((error) => {
-    res.send(error);
+    res.status(400).send(error);
+  });
+});
+
+app.get('/users', (req, res) => {
+  User.find({}).then((users) => {
+    res.send(users);
+  }).catch((error) => {
+    res.status(400).send(error);
+  });
+});
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id).then((user) => {
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  }).catch((error) => {
+    res.status(500).send(error);
+  });
+});
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+  task.save().then((result) => {
+    res.send(result);
+  }).catch((error) => {
+    res.status(400).send(error);
   });
 });
 
--- nodeJS-course/task-manager/src/models/task.js
+++ nodeJS-course/task-manager/src/models/task.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const Task = mongoose.model('task', {
   description: {
     type: String,
