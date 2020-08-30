240340c531e9626c01ba3142c55e9713ea52093c
Erick Castañeda Martínez <erickce40@gmail.com>
2020-08-30 00:20:04 -0500

    refactoring using async async await

--- nodeJS-course/task-manager/src/index.js
+++ nodeJS-course/task-manager/src/index.js
@@ -8,42 +8,72 @@ const port = process.env.PORT || 3000; // Port for Heroku deployment
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
-  user.save().then((result) => {
-    res.send(result);
-  }).catch((error) => {
+
+  try {
+    await user.save(user);
+    res.status(201).send(user);
+  } catch (error) {
     res.status(400).send(error);
-  });
+  }
 });
 
-app.get('/users', (req, res) => {
-  User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
     res.send(users);
-  }).catch((error) => {
+  } catch (error) {
     res.status(400).send(error);
-  });
+  }
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
-  User.findById(_id).then((user) => {
+
+  try {
+    const user = await User.findById(_id);
     if (!user) {
       return res.status(404).send();
     }
     res.send(user);
-  }).catch((error) => {
+  } catch (error) {
     res.status(500).send(error);
-  });
+  }
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
-  task.save().then((result) => {
-    res.send(result);
-  }).catch((error) => {
+
+  try {
+    await task.save(task);
+    res.status(201).send(task);
+  } catch (error) {
     res.status(400).send(error);
-  });
+  }
+});
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    return res.status(500).send();
+  }
 });
 
 app.listen(port, () => {
