e1b98550341ff38e7613c286bde50059b1a9afd9
Erick Castañeda Martínez <erickce40@gmail.com>
2020-09-13 21:56:30 -0500

    hiding sensible data

--- nodeJS-course/task-manager/src/models/user.js
+++ nodeJS-course/task-manager/src/models/user.js
@@ -67,6 +67,13 @@ userSchema.methods.generateAuthToken = async function () {
   return token;
 };
 
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  return userObject;
+};
 /**
  *
  * Static are accesible on the MODEL.
